09c5356b016a5cc37ac949af144ab8c4b04be4de
EnW07 <ajjsdhjs@gmail.com>
2026-02-05 15:52:55 +0400

    Border limit for player
    Made it so a player cannot pass borders and go out of the visible screen by adding check in update() in DetectCollisions script and also tags for these same borders

--- Assets/Scripts/DetectCollisions.cs
+++ Assets/Scripts/DetectCollisions.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class DetectCollisions : MonoBehaviour
 {
 
+    public Vector3 borderMax = new Vector3(-6, 3.5f, 0);
+    public Vector3 borderMin = new Vector3(-6, -3.5f, 0);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,16 +15,21 @@ public class DetectCollisions : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameObject.transform.position.y >= borderMax.y)
+        {
+            Debug.Log("yo");
+            transform.position = borderMax;
+        }
+        else if (gameObject.transform.position.y <= borderMin.y)
+        {
+            Debug.Log("youltra");
+            transform.position = borderMin;
+        }
     }
 
 
     public void OnTriggerEnter2D(Collider2D other)
     {
-        
-
-
-
         if (gameObject.CompareTag("Player"))
         {
             PlayerHealth.Instance.PlayerHealthCounter(other);
